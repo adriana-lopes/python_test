7983d6205a3a5a68bdce0ee434f3998292721de3
Adriana_lopes <lopes2017cf@gmail.com>
2025-05-06 16:20:56 -0300

    Verificação do servidor Urban Routes.

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
-def in_autotests_we_trust(a, b):
-    if a == b:
-        print('Pass')
-    else:
-        print('Fail')
-in_autotests_we_trust(10, '10')
+import data
+import helpers
 
-in_autotests_we_trust(0, False)
+
+class TestUrbanRoutes:
+    def test_set_route(self):
+        #Adicionar em S8
+        print("função criada para definir a rota")
+        pass
+
+    def test_select_plan(self):
+        #Adicionar em S8
+        print("função criada para selecionar o plano")
+        pass
+
+    def test_fill_phone_number(self):
+        #Adicionar em S8
+        print("função criada para preencher o número de telefone")
+        pass
+
+    def test_fill_card(self):
+        #Adicionar em S8
+        print("função criada para preencher o cartão")
+        pass
+
+    def test_comment_for_driver(self):
+        #Adicionar em S8
+        print("função criada para comentar para o motorista")
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        #Adicionar em S8
+        print("função criada para pedir cobertor e lenços")
+        pass
+
+    def test_order_2_ice_creams(self):
+        print("função criada para pedir 2 sorvetes")
+        for i in range(2):
+            #Adicionar em s8
+        pass
+
+    def test_car_search_model_appears(self):
+        #Adicionar em S8
+        print("função criada para verificar se o modelo do carro aparece na busca")
+        pass
